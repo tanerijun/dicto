dc238726b1a698a603911b446f9b2e00b6baef44
tanerijun <tanerivince@gmail.com>
2023-07-16 00:44:56 +0800

    App description

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,8 +2,11 @@ import type { V2_MetaFunction } from "@remix-run/cloudflare";
 
 export const meta: V2_MetaFunction = () => {
 	return [
-		{ title: "New Remix App" },
-		{ name: "description", content: "Welcome to Remix!" },
+		{ title: "Word Bliss" },
+		{
+			name: "description",
+			content: "Elevate Your Vocabulary, Elevate Your Bliss",
+		},
 	];
 };
 
