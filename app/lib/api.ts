f275f3538733b442dc309d8a41d00c98d20128bf
tanerijun <tanerivince@gmail.com>
2023-07-16 20:07:44 +0800

    Cleaning up

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -4,15 +4,9 @@ import { WordDefinitionsSchema } from "./schema";
 const BASE_URL = "https://api.dictionaryapi.dev/api/v2/entries/en";
 
 export async function getWordDefinitions(word: string) {
-	const response = await fetch(`${BASE_URL}/${word}`);
-
-	if (!response.ok) {
-		throw new Error("Problem fetching definitions. Try again later!");
-	}
-
-	const json = await response.json();
-
 	try {
+		const response = await fetch(`${BASE_URL}/${word}`);
+		const json = await response.json();
 		const data = z.array(WordDefinitionsSchema).parse(json)[0];
 		return data;
 	} catch {
