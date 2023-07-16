ace4a5d3f53fbe2f37e3e20cf2d56a03647ffe85
tanerijun <tanerivince@gmail.com>
2023-07-16 21:51:29 +0800

    We can now switch between available phonetics

--- app/components/ResultView.tsx
+++ app/components/ResultView.tsx
@@ -1,38 +1,57 @@
+import { useState } from "react";
 import { type WordDefinitions } from "~/lib/schema";
 import { capitalize } from "~/lib/utils";
+
+const Header = ({ definitions }: { definitions: WordDefinitions }) => {
+	const phonetics = definitions.phonetics
+		? definitions.phonetics
+				.map((phonetic) => ({
+					text: phonetic.text ? phonetic.text : undefined,
+					audioUrl: phonetic.audio ? phonetic.audio : undefined,
+				}))
+				.filter((phonetic) => phonetic.text)
+		: definitions.phonetic
+		? [{ text: definitions.phonetic, audioUrl: undefined }]
+		: [];
+
+	// TODO: handle case when phonetic text is not available, like with word "Hello"
+	// The last change should fixed it
+
+	const [phoneticIndex, setPhoneticIndex] = useState(0);
+
+	const handleClick = () => {
+		const nextPhoneticIndex =
+			phoneticIndex === phonetics.length - 1 ? 0 : phoneticIndex + 1;
+		setPhoneticIndex(nextPhoneticIndex);
+	};
+
+	return (
+		<header className="flex justify-between">
+			<div>
+				<h1 className="text-5xl font-bold">{capitalize(definitions.word)}</h1>
+				<div className="flex">
+					<p className="text-violet-500">{phonetics[phoneticIndex].text}</p>
+					{/* TODO: Button to switch among the phonetics */}
+					{phonetics.length > 1 && (
+						<button className="border border-red-500 p-1" onClick={handleClick}>
+							{phoneticIndex}
+						</button>
+					)}
+				</div>
+			</div>
+			{phonetics[phoneticIndex].audioUrl && <button>Play</button>}
+		</header>
+	);
+};
 
 export const ResultView = ({
 	definitions,
 }: {
 	definitions: WordDefinitions;
 }) => {
-	// TODO: consider putting phonetics in its own component
-	const phonetics = definitions.phonetics
-		? definitions.phonetics.map((phonetic) => ({
-				text: phonetic.text,
-				audioUrl: phonetic.audio ? phonetic.audio : undefined,
-		  }))
-		: definitions.phonetic
-		? [{ text: definitions.phonetic, audioUrl: undefined }]
-		: [];
-
 	return (
 		<article>
-			<header className="flex justify-between">
-				<div>
-					<h1 className="text-5xl font-bold">{capitalize(definitions.word)}</h1>
-					<div className="flex">
-						{phonetics.map((phonetic) => (
-							<p key={phonetic.text} className="text-violet-500">
-								{phonetic.text}
-							</p>
-						))}
-						{/* TODO: Button to switch among the phonetics */}
-						<button className="border border-red-500 p-1">+</button>
-					</div>
-				</div>
-				<button>Play</button>
-			</header>
+			<Header definitions={definitions} />
 		</article>
 	);
 };
