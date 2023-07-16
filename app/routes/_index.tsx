c5470b819013e3a698e7f027b36a7f2273d53c7e
tanerijun <tanerivince@gmail.com>
2023-07-16 12:57:54 +0800

    Add form

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,7 +13,19 @@ export const meta: V2_MetaFunction = () => {
 export default function Index() {
 	return (
 		<main className="mx-auto max-w-3xl px-4">
-			<h1 className=" bg-red-400">Hello world</h1>
+			<form id="search-form">
+				<input
+					type="search"
+					name="query"
+					id="search-bar"
+					defaultValue={"QUERY PARAM"}
+				/>
+				<button type="submit">Search</button>
+			</form>
+
+			<output htmlFor="search-bar" form="search-form">
+				TODO
+			</output>
 		</main>
 	);
 }
