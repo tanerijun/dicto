25784952792f4f65bdbd1f94c9fad256e965833d
tanerijun <tanerivince@gmail.com>
2023-07-18 15:06:55 +0800

    Update error boundary

--- app/root.tsx
+++ app/root.tsx
@@ -25,7 +25,7 @@ export const action = async ({ request }: ActionArgs) => {
 	return redirect(`/${word}`);
 };
 
-export default function App() {
+const Document = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<html lang="en">
 			<head>
@@ -35,12 +35,20 @@ export default function App() {
 				<Links />
 			</head>
 			<body>
-				<Outlet />
+				{children}
 				<ScrollRestoration />
 				<Scripts />
 				<LiveReload />
 			</body>
 		</html>
+	);
+};
+
+export default function App() {
+	return (
+		<Document>
+			<Outlet />
+		</Document>
 	);
 }
 
@@ -50,50 +58,42 @@ export function ErrorBoundary() {
 	console.error(error);
 
 	return (
-		<html>
-			<head>
-				<title>Oh no!</title>
-				<Meta />
-				<Links />
-			</head>
-			<body>
-				<main className="flex h-screen flex-col items-center justify-center">
-					<h1 className="text-4xl font-bold">Application Error</h1>
-					<p>This is most likely our issue</p>
-					<p className="my-10 select-none text-9xl">😞</p>
-					<a
-						href="https://github.com/tanerijun/word-bliss/issues/new"
-						target="_blank"
-						rel="noreferrer"
-						className="text-red-400 underline"
-					>
-						Report the issue
-					</a>
-					<details className="mt-8">
-						<summary className="mb-4 cursor-pointer text-sm">
-							View error message
-						</summary>
-						{isRouteErrorResponse(error) ? (
-							<div className="flex flex-col bg-red-50 p-8 text-red-400">
-								<h2>
-									{error.status} {error.statusText}
-								</h2>
-								<p>{error.data}</p>
-							</div>
-						) : error instanceof Error ? (
-							<div className="flex flex-col bg-red-50 p-8 text-red-400">
-								<h2>Error</h2>
-								<p>{error.message}</p>
-							</div>
-						) : (
-							<div className="flex flex-col bg-red-50 p-8 text-red-400">
-								<h2>"Unknown error"</h2>
-							</div>
-						)}
-					</details>
-				</main>
-				<Scripts />
-			</body>
-		</html>
+		<Document>
+			<main className="flex h-screen flex-col items-center justify-center">
+				<h1 className="text-4xl font-bold">Application Error</h1>
+				<p>This is most likely our issue</p>
+				<p className="my-10 select-none text-9xl">😞</p>
+				<a
+					href="https://github.com/tanerijun/word-bliss/issues/new"
+					target="_blank"
+					rel="noreferrer"
+					className="text-red-400 underline"
+				>
+					Report the issue
+				</a>
+				<details className="mt-8">
+					<summary className="mb-4 cursor-pointer text-sm">
+						View error message
+					</summary>
+					{isRouteErrorResponse(error) ? (
+						<div className="flex flex-col bg-red-50 p-8 text-red-400">
+							<h2>
+								{error.status} {error.statusText}
+							</h2>
+							<p>{error.data}</p>
+						</div>
+					) : error instanceof Error ? (
+						<div className="flex flex-col bg-red-50 p-8 text-red-400">
+							<h2>Error</h2>
+							<p>{error.message}</p>
+						</div>
+					) : (
+						<div className="flex flex-col bg-red-50 p-8 text-red-400">
+							<h2>"Unknown error"</h2>
+						</div>
+					)}
+				</details>
+			</main>
+		</Document>
 	);
 }
--- app/routes/_home.$word.tsx
+++ app/routes/_home.$word.tsx
@@ -1,5 +1,10 @@
 import { json, type LoaderArgs } from "@remix-run/cloudflare";
-import { useLoaderData } from "@remix-run/react";
+import {
+	isRouteErrorResponse,
+	useLoaderData,
+	useParams,
+	useRouteError,
+} from "@remix-run/react";
 import { getWordDefinitionsFromAPI } from "~/lib/api";
 import { APIWordDefinitionsSchema } from "~/lib/schema";
 import { cleanWordDefinitions } from "~/lib/utils";
@@ -32,4 +37,21 @@ export default function HomeWordPage() {
 	console.log(data);
 
 	return <div>{data.word}</div>;
+}
+
+// TODO: Styling
+export function ErrorBoundary() {
+	const params = useParams();
+	const error = useRouteError();
+
+	if (isRouteErrorResponse(error) && error.status === 404) {
+		return <div>No definition found for the word: "{params.word}"?</div>;
+	}
+
+	return (
+		<div>
+			There was an error loading definition for the word: "${params.word}".
+			Sorry.
+		</div>
+	);
 }
