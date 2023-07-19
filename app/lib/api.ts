2db461a622d0f6e4c0d759ee00d9413d1abd1a8b
tanerijun <tanerivince@gmail.com>
2023-07-20 00:42:16 +0800

    Clean up

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,6 +1,5 @@
 const BASE_URL = "https://api.dictionaryapi.dev/api/v2/entries/en";
 
-// TODO: If I decide to implement nested route, consider throwing an error instead of null
 export async function getWordDefinitionsFromAPI(word: string) {
 	const response = await fetch(`${BASE_URL}/${word}`);
 	if (!response.ok) {
